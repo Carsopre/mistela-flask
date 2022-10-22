730a6d6d149341c3b8738572fd58fa6f83baf1f3
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-10-22 19:14:36 +0200

    feat(login;signup): Added auth to both login and setup. Adapted templates

--- mistelaflask/auth.py
+++ mistelaflask/auth.py
@@ -1,16 +1,58 @@
-from flask import Blueprint, render_template
-from . import db
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
 
-auth = Blueprint('auth', __name__)
+from mistelaflask import db
+from mistelaflask.models import User
 
-@auth.route('/login')
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login")
 def login():
     return render_template("login.html")
 
-@auth.route('/signup')
+
+@auth.route("/login", methods=["POST"])
+def login_post():
+    # login code goes here
+    return redirect(url_for("main.profile"))
+
+
+@auth.route("/signup")
 def signup():
     return render_template("signup.html")
 
-@auth.route('/logout')
+
+@auth.route("/signup", methods=["POST"])
+def signup_post():
+    # code to validate and add user to database goes here
+    email = request.form.get("email")
+    name = request.form.get("name")
+    password = request.form.get("password")
+
+    user = User.query.filter_by(
+        email=email
+    ).first()  # if this returns a user, then the email already exists in database
+
+    if (
+        user
+    ):  # if a user is found, we want to redirect back to signup page so user can try again
+        flash("Email address already exists")
+        return redirect(url_for("auth.signup"))
+
+    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    new_user = User(
+        email=email,
+        name=name,
+        password=generate_password_hash(password, method="sha256"),
+    )
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for("auth.login"))
+
+
+@auth.route("/logout")
 def logout():
-    return 'Logout'
+    return "Logout"
