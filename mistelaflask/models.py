f7e11b8c36bbae1c8bac425563247ddf1892a9fe
Carles S. Soriano Perez <sorianoperez.carles@gmail.com>
2022-10-23 12:08:57 +0200

    feat(models.py): Added new tables

--- mistelaflask/models.py
+++ mistelaflask/models.py
@@ -7,6 +7,32 @@ class User(UserMixin, db.Model):
     id = db.Column(
         db.Integer, primary_key=True
     )  # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    password = db.Column(db.String(100))
+    admin = db.Column(db.Boolean, default=False, nullable=False)
+
+
+class MainEvent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    description = db.Column(db.Text)
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    description = db.Column(db.Text)
+    main_event_id = db.Column(db.Integer, db.ForeignKey("mainevent.id"))
+
+
+class UserInvitation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    remarks = db.Column(db.Text)
+    main_event_id = db.Column(db.Integer, db.ForeignKey("mainevent.id"))
+
+
+class UserEventInvitation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    guest = db.Column(db.Integer, db.ForeignKey("user.id"))
+    event = db.Column(db.Integer, db.ForeignKey("event.id"))
+    response = db.Column(db.Boolean, default=False, nullable=False)
