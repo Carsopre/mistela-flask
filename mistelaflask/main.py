cd8778ac5a790f55b718930fac36cb932e086d33
Carles S. Soriano Perez <sorianoperez.carles@gmail.com>
2022-11-05 16:35:50 +0100

    feat(guest_responses.html): Added page for a guest to manage their invitations

--- mistelaflask/main.py
+++ mistelaflask/main.py
@@ -22,3 +22,28 @@ def profile():
 def events():
     _events = models.Event.query.all()
     return render_template("events.html", events=_events)
+
+
+@main.route("/responses")
+@login_required
+def responses():
+    # current_user
+    _user_invitations = []
+    for _invitation in models.UserEventInvitation.query.filter_by(
+        guest=current_user.id
+    ):
+        _event = models.Event.query.filter_by(id=_invitation.id).first()
+        _user_invitations.append(
+            dict(
+                event_name=_event.name,
+                max_adults=current_user.max_adults,
+            )
+        )
+
+    return render_template("guest_responses.html", user_invitations=_user_invitations)
+
+
+@main.route("/responses", methods=["UPDATE"])
+@login_required
+def update_responses():
+    pass
