5d6d3338c62f88b73a3b7c6140413cf60a43fe6c
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-11-08 21:24:04 +0100

    chore(models.py;db_scripts.py): Reworked on the event definition to include start and duration of the event. Defined test data for starting to use the tool

--- mistelaflask/models.py
+++ mistelaflask/models.py
@@ -16,6 +16,8 @@ class User(UserMixin, db.Model):
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(1000))
+    start_time = db.Column(db.DateTime)
+    duration = db.Column(db.Integer)  # In Minutes
     description = db.Column(db.Text)
 
 
