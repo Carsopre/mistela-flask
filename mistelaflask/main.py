4bee067ef08c9ed090da1317042176a0655c12f0
Carles S. Soriano Perez <sorianoperez.carles@gmail.com>
2022-11-12 16:16:25 +0100

    ci: created wsgi file

--- mistelaflask/main.py
+++ mistelaflask/main.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import Blueprint, Flask, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
 from mistelaflask import db, models
