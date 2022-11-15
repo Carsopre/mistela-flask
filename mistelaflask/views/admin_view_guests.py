25fe4f1131ae19600a6633bc451eeb908e4287e3
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-11-15 21:29:44 +0100

    feat(admin/guests): We can now do full CRUD operations on Guests (users without admin rights)

--- mistelaflask/views/admin_view_guests.py
+++ mistelaflask/views/admin_view_guests.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
-from werkzeug.security import generate_password_hash
 
 from mistelaflask import db, models
 from mistelaflask.views.admin_view_protocol import AdminViewProtocol
@@ -70,9 +69,17 @@ class AdminViewGuests(AdminViewProtocol):
         if not current_user.admin:
             return redirect(url_for("index"))
         _guest = models.User.query.filter_by(id=guest_id).first()
+        _events = models.Event.query.all()
+        _invitations = []
+        for _event in _events:
+            _is_invited = db.session.query(
+                models.UserEventInvitation.query.filter_by(
+                    guest=_guest, event=_event
+                ).exists()
+            ).scalar()
+            _invitations.append((_event, _is_invited))
         return self._render_guests_template(
-            "admin_guests_detail.html",
-            event=_guest,
+            "admin_guests_detail.html", guest=_guest, invitations=_invitations
         )
 
     @login_required
@@ -86,35 +93,26 @@ class AdminViewGuests(AdminViewProtocol):
         flash(f"User {_name} has been removed.", category="danger")
         return redirect(url_for("admin.guests_list"))
 
-    @login_required
-    def _create_view(self):
-        if not current_user.admin:
-            return redirect(url_for("index"))
-        name = request.form.get("name")
-        password = request.form.get("password")
-        max_adults = request.form.get("max_adults")
-        _user = models.User.query.filter_by(name=name).first()
-        if _user:
-            flash("A user with this name already exists.")
-            return redirect(url_for("mistela_admin.guests"))
-        _new_user = models.User(
-            name=name,
-            max_adults=max_adults,
-            password=generate_password_hash(password, method="sha256"),
-        )
-        db.session.add(_new_user)
-        db.session.commit()
-        _user_invitation = models.UserEventInvitation(guest=_new_user.id)
-        db.session.add(_user_invitation)
-        db.session.commit()
+    def _set_guest_values(self, guest: models.User):
+        guest.username = request.form.get("username")
+        guest.name = request.form.get("name")
+        guest.max_adults = request.form.get("max_adults", 2)
+        guest.otp = request.form.get("otp")
+        return guest
+
+    def _set_guest_invitations(self, guest: models.User):
         for _event in models.Event.query.all():
-            if request.form.get(f"event_{_event.id}", type=bool, default=False):
-                _new_invitation = models.UserEventInvitation(
-                    guest=_new_user, event=_event
-                )
-                db.session.add(_new_invitation)
-                db.session.commit()
-        return redirect(url_for("mistela_admin.guests"))
+            _form_value = True if request.form.get(f"event_{_event.name}") else False
+            _exists = models.UserEventInvitation.query.filter_by(
+                event=_event, guest=guest
+            ).first()
+            if _form_value and not _exists:
+                db.session.add(models.UserEventInvitation(guest=guest, event=_event))
+            elif not _form_value and _exists:
+                models.UserEventInvitation.query.filter_by(
+                    event=_event, guest=guest
+                ).delete()
+            db.session.commit()
 
     @login_required
     def _update_view(self, guest_id: int):
@@ -125,9 +123,10 @@ class AdminViewGuests(AdminViewProtocol):
             flash("Guest not found.", category="danger")
             return redirect(url_for("admin.guests_list"))
 
-        _guest.name = request.form.get("name")
-        _guest.description = request.form.get("description")
+        self._set_guest_values(_guest)
         db.session.commit()
+        self._set_guest_invitations(_guest)
+
         flash(f"Guest '{_guest.id}' updated", category="info")
         return redirect(url_for("admin.guests_list"))
 
@@ -135,21 +134,27 @@ class AdminViewGuests(AdminViewProtocol):
     def _add_view(self):
         if not current_user.admin:
             return redirect(url_for("index"))
-        return self._render_guests_template("admin_guests_add.html")
+
+        _invitations = []
+        for _event in models.Event.query.all():
+            _invitations.append((_event, False))
+        return self._render_guests_template(
+            "admin_guests_add.html", guest=models.User(), invitations=_invitations
+        )
 
     @login_required
     def _create_view(self):
         if not current_user.admin:
             return redirect(url_for("index"))
-        name = request.form.get("name")
-        description = request.form.get("description")
 
-        _guest = models.User.query.filter_by(name=name).first()
-        if _guest:
+        _new_guest = models.User()
+        self._set_guest_values(_new_guest)
+        if models.User.query.filter_by(username=_new_guest.username).first():
             flash("A guest with this name already exists.", category="danger")
-            return redirect(url_for("guests_create"))
-        _new_guest = models.User(name=name, description=description)
+            return redirect(url_for("admin.guests_add", guest=_new_guest))
         db.session.add(_new_guest)
         db.session.commit()
-        flash(f"Added guest '{name}'.", category="success")
+        self._set_guest_invitations(_new_guest)
+
+        flash(f"Added guest '{_new_guest.username}'.", category="success")
         return redirect(url_for("admin.guests_list"))
