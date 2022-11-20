f214940b1dbe19fd6c9c118c08d8d16a4f7462bf
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-11-20 19:37:14 +0100

    ci(wsgi): Fixed wsgi

--- wsgi.py
+++ wsgi.py
@@ -58,30 +58,35 @@ def init_test_db() -> Flask:
         events = [
             models.Event(
                 name="reception",
+                icon="fa fa-signs-post",
                 start_time=_correct_datetime(hour=16, minute=00),
                 duration=30,
                 description="Guests can gather at the venue and enjoy a welcome drink.",
             ),
             models.Event(
                 name="ceremony",
+                icon="fa fa-ring fa-rotate-90",
                 start_time=_correct_datetime(hour=16, minute=30),
                 duration=45,
                 description="Wedding ceremony with ring exchange.",
             ),
             models.Event(
                 name="cake and toast",
+                icon="fa fa-champagne-glasses",
                 start_time=_correct_datetime(hour=17, minute=15),
                 duration=75,
                 description="A toast with cava and cake to celebrate the moment.",
             ),
             models.Event(
                 name="dinner",
+                icon="fa fa-utensils",
                 description="BBQ Buffet with dessert table.",
                 start_time=_correct_datetime(hour=18, minute=30),
                 duration=150,
             ),
             models.Event(
                 name="party",
+                icon="fa fa-music",
                 description="We will play some musice to dance and finish the day with a warm party",
                 start_time=_correct_datetime(hour=21),
                 duration=4 * 60,
@@ -100,7 +105,7 @@ def init_test_db() -> Flask:
         )
         db.session.add(_night_invitation)
         db.session.commit()
-    return _app
+        return _app
 
 
 os.environ["SECRET_KEY"] = secrets.token_hex(16)  # Required environment variable.
